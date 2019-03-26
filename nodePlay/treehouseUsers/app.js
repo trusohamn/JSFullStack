d3354b04d6b43c8f0f0be36be3ae41df20d1878e
trusohamn <marta.trusohamn@gmail.com>
2019-03-26 03:50:27 -0700

    parsing json

--- nodePlay/treehouseUsers/app.js
+++ nodePlay/treehouseUsers/app.js
@@ -3,32 +3,38 @@
 
 //import https module
 const https = require('https');
-const username = 'martatrusohamn';
 
 function userToConsole(username, badgeCount, point) {
     const message = `${username} has ${badgeCount} total badges and ${point} points in JS`;
     console.log(message);
 }
 
-//connecto to API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`,
-response => {
-    console.dir(response.statusCode);
-    //gather all packages of text
-    let body = "";
-    //read data
-    response.on('data', data => {
-        //console.log('buffer: ' + data);
-        //console.log('string: ' + data.toString());
-        body += data.toString();
+function getTreehouseProfile(username){
+    //connecto to API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`,
+    response => {
+            //console.dir(response.statusCode);
+        //gather all packages of text
+        let body = "";
+        //read data
+        response.on('data', data => {
+                //console.log('buffer: ' + data);
+                //console.log('string: ' + data.toString());
+            body += data.toString();
 
-    })
-    //finish on end event
-    response.on('end', () => {
-        console.log(body);
-    })
-    //parse json
-
-});
+        })
+        //finish on end event
+        response.on('end', () => {
+                //console.log(body);
+            //parse json from string
+            const userData = JSON.parse(body);
+                //console.dir(userData);
+            userToConsole(username, userData.badges.length, userData.points.JavaScript)
+        })
+    });
+}
+const users = ['martatrusohamn'];
+//users.forEach(username => getTreehouseProfile(username));
+users.forEach(getTreehouseProfile);
 
 
