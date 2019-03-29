6e02d677580a5ad4f40549fae98f4de178e786f5
trusohamn <marta.trusohamn@gmail.com>
2019-03-29 01:08:54 -0700

    loading css

--- nodePlay/firstDynamicNode/app.js
+++ nodePlay/firstDynamicNode/app.js
@@ -1,12 +1,27 @@
 var http = require('http');
 
 //create web server
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
+http.createServer(function (request, response) {  
+    if (request.url === '/style.css') {
+        //loading the css, call the url /stye.css
+        console.log('loading css')
+        
+        response.writeHead(200, {'Content-type' : 'text/css'});
 
-  view(response, 'index');
-  response.end();
-  
+        var fileContents = fs.readFileSync('./css/style.css', {encoding: 'utf8'});
+        response.write(fileContents);
+
+        response.end();
+    }
+
+    else {
+        console.log('loading html');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+
+        view(response, 'index');
+        response.end();
+  }
+
 }).listen(3000, '127.0.0.1');
 console.log('Server running at localhost');
 
@@ -19,6 +34,4 @@ function view(response, htmlFile){
   var fileContents = fs.readFileSync('./views/' + htmlFile + '.html', {encoding: "utf8"});
   //Write out the contents to the response
   response.write(fileContents);
-
-
 }
